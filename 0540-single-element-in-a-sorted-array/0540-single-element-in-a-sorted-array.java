df22ec16672505d1a774e3af01deae9b0bb77ea6
pushkar-chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2024-09-13 21:40:14 +0530

    Time: 0 ms (100%), Space: 49.9 MB (92.67%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,9 +1,43 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-      int x=0;
-      for(int i=0;i<nums.length;i++){
-        x=x^nums[i];
-      }
-      return x;
+    //   int x=0;
+    //   for(int i=0;i<nums.length;i++){
+    //     x=x^nums[i];
+    //   }
+    //   return x;
+
+    int start=0;
+    int end=nums.length-1;
+    while(start<end){
+        int mid=start+(end-start)/2;
+        //check rigth side me even hai ya odd
+        boolean isEven;
+        if((end-mid)%2==0){
+            isEven=true;
+        }
+        else{
+            isEven=false;
+        }
+        //check adjuscent element
+
+        if(nums[mid]==nums[mid+1]){
+            if(isEven){
+                start=mid+2;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        else{//nums[mid]!=nums[mid+1]
+            if(isEven){
+                end=mid;
+            }
+            else{
+                start=mid+1;
+            }
+
+        }
+    }
+    return nums[end];
     }
 }
