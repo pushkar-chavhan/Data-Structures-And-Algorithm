6b58a27c158603ba30a7065d2db24bc694bd52d3
pushkar-chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2024-09-26 22:42:35 +0530

    Time: 17 ms (49.62%), Space: 46.4 MB (25.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,29 +4,15 @@ class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
         int[] nge = new int[n];
-        
-        // Initialize all values to -1 (assuming no greater element found)
-        for (int i = 0; i < n; i++) {
-            nge[i] = -1;
-        }
-        
-        Stack<Integer> stack = new Stack<>();
-        
-        // We traverse the array twice due to circular nature
-        for (int i = 0; i < 2 * n; i++) {
-            int index = i % n;
-            
-            // Maintain decreasing stack, find next greater element
-            while (!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
-                nge[stack.pop()] = nums[index];
+        Stack<Integer>st=new Stack<>();
+        for(int i=2*n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek()<=nums[i%n]){
+                st.pop();
             }
-            
-            // Only push indices for first pass, not during the second traversal
-            if (i < n) {
-                stack.push(index);
+            if(i<n){
+                nge[i]=st.empty() ? -1:st.peek();
             }
-        }
-        
-        return nge;
+            st.push(nums[i%n]);
+        }return nge;
     }
 }
