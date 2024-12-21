3a36fcd5f1cfc2a88cf4e08dba220c15309c9fe0
pushkar-chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2024-12-21 21:50:33 +0530

    Time: 32 ms (75.25%), Space: 17.9 MB (12.54%) - LeetHub

---
+++ 1970-sorting-the-sentence/1970-sorting-the-sentence.py
@@ -0,0 +1,6 @@
+class Solution:
+    def sortSentence(self, s: str) -> str:
+        words=s.split()
+        sorted_words=sorted(words,key=lambda word:int(word[-1]))
+        return " ".join(word[:-1] for word in sorted_words)
+       
