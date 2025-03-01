82c9e2820bec0310f9cf01ac942dae06acab85f0
pushkar-chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-03-01 18:31:44 +0530

    Time: 0 ms (100%), Space: 40.4 MB (47.89%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,9 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
+        // if(n==0) return 0;
+        // if(n==1) return 1;
         
-            return fib(n-1)+fib(n-2);
-        
+        //     return fib(n-1)+fib(n-2);
+
+        if(n<=1){
+            return n;
+        }
+        int map[]=new int[n+1];
+        map[0]=0;
+        map[1]=1;
+        for(int i=2;i<=n;i++){
+            map[i]=map[i-1]+map[i-2];
+        }
+        return map[n];
     }
 }
