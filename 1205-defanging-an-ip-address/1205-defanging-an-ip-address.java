56cc696f8bf6eeb4e5d34a70c5f631a64fc1a692
pushkar-chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-06-26 14:52:58 +0530

    Time: 5 ms (26.8%), Space: 42.4 MB (5.02%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -1,15 +1,14 @@
 class Solution {
     public String defangIPaddr(String address) {
-        int n=address.length();
-        String s="";
-        for(int i=0;i<n;i++){
-            char ch =address.charAt(i);
-            if(ch!='.'){
-                s=s+ch;
-            }else{
-                s=s+"[.]";
+        String res="";
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)!='.'){
+                res+=address.charAt(i);
+            }
+            else if(address.charAt(i)=='.'){
+                res+="[.]";
             }
         }
-        return s;
+        return res;
     }
 }
