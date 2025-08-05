301eebc7f50aa5818e2af9d0914869c9e1649fc5
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-05 21:36:42 +0530

    Time: 3 ms (99.81%), Space: 65.6 MB (13.5%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.java
@@ -10,19 +10,37 @@
  */
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
-        if(head==null || head.next==null){
-            return null;
+    //     if(head==null || head.next==null){
+    //         return null;
+    //     }
+    //  ListNode slow=head;   
+    //  ListNode fast=head;
+    //  fast=head.next.next;
+    //  while(fast!=null && fast.next!=null){
+    //     slow=slow.next;
+    //     fast=fast.next.next;
+    //  }   
+    //  if(slow.next!=null){
+    //     slow.next=slow.next.next;
+    //  }
+    //  return head;
+
+
+    //using prev pointer
+    
+    if(head==null ||head.next==null){
+        return null;
+    }
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null && fast.next!=null){
+           prev=slow;
+           slow=slow.next;
+           fast=fast.next.next;
         }
-     ListNode slow=head;   
-     ListNode fast=head;
-     fast=head.next.next;
-     while(fast!=null && fast.next!=null){
-        slow=slow.next;
-        fast=fast.next.next;
-     }   
-     if(slow.next!=null){
-        slow.next=slow.next.next;
-     }
-     return head;
+        prev.next=slow.next;
+    
+    return head;
     }
 }
