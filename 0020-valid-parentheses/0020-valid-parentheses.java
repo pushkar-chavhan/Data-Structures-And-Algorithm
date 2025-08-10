1f920eed86d241ce287239a5a8f517946dcc78d4
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-10 22:40:21 +0530

    Time: 2 ms (96.57%), Space: 41.9 MB (47.5%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,22 +1,39 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character>stack=new Stack<>();
-        for(char ch:s.toCharArray()){
-            if(ch=='('){
-                stack.push(')');
-            }
-            else if(ch=='{'){
-                stack.push('}');
-            }
-            else if(ch=='['){
-                stack.push(']');
-            }
-            else if(stack.isEmpty() || stack.pop()!=ch){
+        Stack<Character>st=new Stack<>();
+        // for(char ch:s.toCharArray()){
+        //     if(ch=='('){
+        //         stack.push(')');
+        //     }
+        //     else if(ch=='{'){
+        //         stack.push('}');
+        //     }
+        //     else if(ch=='['){
+        //         stack.push(']');
+        //     }
+        //     else if(stack.isEmpty() || stack.pop()!=ch){
 
-                return false;
-            }
+        //         return false;
+        //     }
 
+        // }
+        // return stack.isEmpty();
+        for(char it:s.toCharArray()){
+            if(it=='('||it=='['||it=='{'){
+                st.push(it);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char ch=st.pop();
+                if(it==')'&&ch=='('||
+                it==']'&&ch=='['||
+                it=='}'&&ch=='{')
+                continue;
+                else{
+                    return false;
+                }
+            }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
