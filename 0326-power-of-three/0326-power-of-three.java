455b06e39b2a0ccfa416b02cbb68dfca2deb6828
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-13 12:49:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -3,8 +3,10 @@ class Solution {
         if(n<=0){
             return false;
         }
-        while(n%3==0){
-            n/=3;
+        while(n>1){
+            if(n%3==0){
+                n/=3;
+            }
         }
         return n==1;
     }
