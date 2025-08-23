94ca223910168932534eb9c0a38662e2af3bb5a7
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-23 13:11:59 +0530

    Time: 27 ms (26.91%), Space: 45.3 MB (21.22%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,21 +1,34 @@
 class Solution {
+    private int fun(int i, int T, int[] nums, int[][] dp) {
+        if (i == 0) {
+            if (T % nums[0] == 0)
+                return T / nums[0];
+            return 1000000000; // large value (acts as infinity)
+        }
+        if (dp[i][T] != -1)
+            return dp[i][T];
+
+        int notTake = fun(i - 1, T, nums, dp);
+        int take = Integer.MAX_VALUE;
+        if (nums[i] <= T) {
+            take = 1 + fun(i, T - nums[i], nums, dp);
+        }
+        return dp[i][T] = Math.min(take, notTake);
+    }
+
     public int coinChange(int[] coins, int amount) {
-        if (amount < 1) {
-            return 0;
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+
+        // initialize dp with -1
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
 
-        int[] minCoinDP = new int[amount + 1];
-        minCoinDP[0] = 0;
-
-        for (int i = 1; i <= amount; i++) {
-            minCoinDP[i] = Integer.MAX_VALUE;
-            for (int coin : coins) {
-                if (i - coin >= 0 && minCoinDP[i - coin] != Integer.MAX_VALUE) {
-                    minCoinDP[i] = Math.min(minCoinDP[i], 1 + minCoinDP[i - coin]);
-                }
-            }
+        int ans = fun(n - 1, amount, coins, dp);
+        if (ans >= 1000000000) {
+            return -1;
         }
-
-        return minCoinDP[amount] == Integer.MAX_VALUE ? -1 : minCoinDP[amount];
+        return ans;
     }
 }
