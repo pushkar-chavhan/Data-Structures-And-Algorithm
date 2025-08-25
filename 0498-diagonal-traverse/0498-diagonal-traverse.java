66abb4ad7c7f324dd091b174b286d829e7b7c7c3
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-25 09:25:00 +0530

    Time: 3 ms (61.54%), Space: 45.8 MB (97.58%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -2,30 +2,51 @@ class Solution {
     public int[] findDiagonalOrder(int[][] mat) {
         int m=mat.length;
         int n=mat[0].length;
-        Map<Integer,List<Integer>>map=new HashMap<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int key=i+j;
-                map.putIfAbsent(key,new ArrayList<>());
-                map.get(key).add(mat[i][j]);
+        // Map<Integer,List<Integer>>map=new HashMap<>();
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         int key=i+j;
+        //         map.putIfAbsent(key,new ArrayList<>());
+        //         map.get(key).add(mat[i][j]);
+        //     }
+        // }
+        // List<Integer>resultList=new ArrayList<>();
+        // boolean flip=true;
+        // for(int k=0;k<=m+n-2;k++){//here k=i+j |m+n-2=3+3-2=4=key
+        //     List<Integer>diagonal=map.get(k);
+        //     if(diagonal==null){
+        //         continue;
+        //     }
+        //     if(flip){
+        //         Collections.reverse(diagonal);//first flip->no flip->flip....
+        //     }
+        //     resultList.addAll(diagonal);
+        //     flip=!flip;
+        // }
+        // int result[]=new int[resultList.size()];
+        // for(int i=0;i<resultList.size();i++){
+        //     result[i]=resultList.get(i);
+        // }
+        // return result;
+        int result[]=new int[m*n];
+        int row=0,col=0;
+        for(int i=0;i<m*n;i++){
+            result[i]=mat[row][col];
+            if((row+col)%2==0){
+                if(col==n-1)row++;
+                else if(row==0)col++;
+                else{
+                    row--;
+                    col++;
+                }
             }
-        }
-        List<Integer>resultList=new ArrayList<>();
-        boolean flip=true;
-        for(int k=0;k<=m+n-2;k++){//here k=i+j |m+n-2=3+3-2=4=key
-            List<Integer>diagonal=map.get(k);
-            if(diagonal==null){
-                continue;
+            else{
+                if(row==m-1)col++;
+                else if(col==0)row++;
+                else{
+                    row++;col--;
+                }
             }
-            if(flip){
-                Collections.reverse(diagonal);
-            }
-            resultList.addAll(diagonal);
-            flip=!flip;
-        }
-        int result[]=new int[resultList.size()];
-        for(int i=0;i<resultList.size();i++){
-            result[i]=resultList.get(i);
         }
         return result;
     }
