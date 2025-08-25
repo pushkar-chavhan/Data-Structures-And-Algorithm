3aefaee58615753523be3f9dc399225a3205426c
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-25 13:26:11 +0530

    Time: 0 ms (100%), Space: 42.1 MB (77.87%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,19 +1,18 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int m=matrix.length;
-        int n=m;
-        
-        //transpose
-        for(int i=0;i<m;i++){
-            for(int j=i;j<n;j++){
+        int n=matrix.length;
+
+        //optimal transpose +reverse
+        for(int i=0;i<n;i++){
+            for(int j=i;j<matrix[0].length;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
             }
         }
-        //reverse transposed matrix
-        
-         for(int i=0;i<m;i++){
+
+        //reverse the matrix
+         for(int i=0;i<n;i++){
             int start=0;
             int end=matrix.length-1;
             while(start<end){
@@ -24,6 +23,17 @@ class Solution {
                 end--;
             }
         }
-      //  return matrix;
+        //brute usingextra space
+        // int [][]rotated=new int[n][n];
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         rotated[j][n-i-1]=matrix[i][j];//n-1-i is beauce i is same 
+        //     }
+        // }
+        // for(int i=0;i<rotated.length;i++){
+        //     for(int j=0;j<rotated.length;j++){
+        //          matrix[i][j]=rotated[i][j];
+        //     }
+        // }
     }
 }
