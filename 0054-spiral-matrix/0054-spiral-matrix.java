20e32c6e6097880b75504cf58c90309169f2cc91
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-25 14:00:05 +0530

    Time: 0 ms (100%), Space: 41.9 MB (15.55%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,52 +1,38 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
-        
-        if (matrix == null || matrix.length == 0) {
-            return result;
-        }
-        
-        int n = matrix.length;
-        int m = matrix[0].length;
-        
-        int row_begin = 0;
-        int row_end = n - 1;
-        int col_begin = 0;
-        int col_end = m - 1;
-        
-        while (row_begin <= row_end && col_begin <= col_end) {
-            
-            // Traverse from left to right
-            for (int j = col_begin; j <= col_end; j++) {
-                result.add(matrix[row_begin][j]);
+        int rw=matrix.length;
+        int rl=matrix[0].length;
+        ArrayList<Integer>ans=new ArrayList<>();
+        int left=0;
+        int right=rl-1;
+        int top=0;
+        int bottum=rw-1;
+        while(left<=right && top<=bottum){
+            //left-right
+            for(int i=left;i<=right;i++){
+                ans.add(matrix[top][i]);
             }
-            row_begin++;  // Move to the next row
-            
-            // Traverse downwards
-            for (int j = row_begin; j <= row_end; j++) {
-                result.add(matrix[j][col_end]);
+            top++;
+            //top-bottum
+            for(int i=top;i<=bottum;i++){
+                ans.add(matrix[i][right]);
             }
-            col_end--;  // Move to the previous column
-            
-            // Traverse from right to left (only if we are still within valid rows)
-            if (row_begin <= row_end) {
-                for (int j = col_end; j >= col_begin; j--) {
-                    result.add(matrix[row_end][j]);
+            right--;
+            //right-left
+            if(top<=bottum){
+                for(int i=right;i>=left;i--){
+                    ans.add(matrix[bottum][i]);
                 }
-                row_end--;  // Move to the previous row
+                bottum--;
             }
-            
-            // Traverse upwards (only if we are still within valid columns)
-            if (col_begin <= col_end) {
-                for (int j = row_end; j >= row_begin; j--) {
-                    result.add(matrix[j][col_begin]);
+            //bottum-top
+            if(left<=right){
+                for(int i=bottum;i>=top;i--){
+                    ans.add(matrix[i][left]);
                 }
-                col_begin++;  // Move to the next column
+                left++;
             }
         }
-        
-        return result;  // Return the spiral order as a list
+        return ans;
     }
 }
