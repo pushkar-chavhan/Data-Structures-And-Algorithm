29884a2d8b299b191d6978890bc4ba4cea8cbc68
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-25 09:26:57 +0530

    Time: 2 ms (99.41%), Space: 45.2 MB (99.92%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.java
+++ 0498-diagonal-traverse/0498-diagonal-traverse.java
@@ -32,7 +32,7 @@ class Solution {
         int row=0,col=0;
         for(int i=0;i<m*n;i++){
             result[i]=mat[row][col];
-            if((row+col)%2==0){
+            if((row+col)%2==0){//for upward arrow
                 if(col==n-1)row++;
                 else if(row==0)col++;
                 else{
@@ -40,7 +40,7 @@ class Solution {
                     col++;
                 }
             }
-            else{
+            else{            //for downward arrow
                 if(row==m-1)col++;
                 else if(col==0)row++;
                 else{
