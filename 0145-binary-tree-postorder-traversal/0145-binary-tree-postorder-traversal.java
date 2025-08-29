a383261e72e6121d8d6ddac71df926995d72117a
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-08-29 14:52:47 +0530

    Time: 0 ms (100%), Space: 41.7 MB (45.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -14,17 +14,38 @@
  * }
  */
 class Solution {
-    static void postOrder(TreeNode root,ArrayList<Integer>list){
-        if(root==null){
-            return;
-        }
-        postOrder(root.left,list);
-        postOrder(root.right,list);
-        list.add(root.val);
-    }
+    // static void postOrder(TreeNode root,ArrayList<Integer>list){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     postOrder(root.left,list);
+    //     postOrder(root.right,list);
+    //     list.add(root.val);
+    // }
     public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer>list=new ArrayList<>();
-        postOrder(root,list);
-        return list;
+        // ArrayList<Integer>list=new ArrayList<>();
+        // postOrder(root,list);
+        // return list;
+
+
+
+        //Using two stacks
+        Stack<TreeNode>st1=new Stack<TreeNode>();
+        Stack<TreeNode>st2=new Stack<TreeNode>();
+        List<Integer>postorder=new ArrayList<>();
+        if(root==null){
+            return postorder;
+        }
+        st1.push(root);//aadhi sgle st1 mdhe add karun takache 
+        while(!st1.isEmpty()){
+            root=st1.pop();
+            st2.add(root);//st1 mdhun kadun st2 mdhe add karache
+            if(root.left!=null) st1.push(root.left);
+            if(root.right!=null) st1.push(root.right);
+        }
+        while(!st2.isEmpty()){
+            postorder.add(st2.pop().val);
+        }
+        return postorder;
     }
 }
