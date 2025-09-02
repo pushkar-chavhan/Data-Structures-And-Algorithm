ca19c185d7a1689a8069b7255de3b7d5c69d3b2b
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-03 00:09:22 +0530

    Time: 227 ms (5.77%), Space: 47.1 MB (8.39%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.java
@@ -1,19 +1,34 @@
 class Solution {
     public String removeDuplicates(String s) {
-        Stack<Character>st=new Stack<>();
-        StringBuilder res=new StringBuilder();
-        for(int i=0;i<s.length();i++){
-            if(st.isEmpty() || st.peek()!=s.charAt(i)){
-                st.push(s.charAt(i));
+        // Stack<Character>st=new Stack<>();
+        // StringBuilder res=new StringBuilder();
+        // for(int i=0;i<s.length();i++){
+        //     if(st.isEmpty() || st.peek()!=s.charAt(i)){
+        //         st.push(s.charAt(i));
+        //     }
+        //     else{
+        //         st.pop();
+        //     }
+        // }
+        // while(!st.isEmpty()){
+        //     res.append(st.peek());
+        //     st.pop();
+        // }
+        // return res.reverse().toString();
+
+        //Using String
+        
+        String result="";
+        
+        for(char ch:s.toCharArray()){
+            int n=result.length();
+            if(n>0 && result.charAt(n-1)==ch){
+                result=result.substring(0,n-1);
             }
             else{
-                st.pop();
+                result+=ch;
             }
         }
-        while(!st.isEmpty()){
-            res.append(st.peek());
-            st.pop();
-        }
-        return res.reverse().toString();
+        return result;
     }
 }
