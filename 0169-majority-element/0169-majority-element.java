cf43f87c0d3bb39ca86cdfa78dbb5a229682a9c9
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-07 20:04:54 +0530

    Time: 12 ms (36.38%), Space: 49.7 MB (77.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,20 +5,48 @@ class Solution {
         //     return nums[n/2];
 
         //Morees voting algo
-        int votes=1;
-        int majority=nums[0];
-        for(int i=1;i<nums.length;i++){
-            if(votes==0){
-                votes++;
-                majority=nums[i];
-            }
-            else if(nums[i]==majority){
-                votes++;
-            }
-            else{
-                votes--;
+        // int votes=1;
+        // int majority=nums[0];
+        // for(int i=1;i<nums.length;i++){
+        //     if(votes==0){
+        //         votes++;
+        //         majority=nums[i];
+        //     }
+        //     else if(nums[i]==majority){
+        //         votes++;
+        //     }
+        //     else{
+        //         votes--;
+        //     }
+        // }
+        // return majority;
+
+
+//Brute:
+        int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     int count=0;
+        //     for(int j=0;j<n;j++){
+        //         if(nums[j]==nums[i]){
+        //             count++;
+        //         }
+        //     }
+        //     if(count>n/2){
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+
+        //HashMap
+        Map<Integer,Integer>mp=new HashMap<>();
+        for(int i=0;i<n;i++){
+            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+            if(entry.getValue()>n/2){
+                return entry.getKey();
             }
         }
-        return majority;
+        return -1;
     }
 }
