e3230b300eca07c5125a301c2231021ec3eed0d1
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-09 10:58:10 +0530

    Time: 6 ms (85.52%), Space: 43 MB (78.82%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,21 +1,42 @@
 class Solution {
     public String reverseWords(String s) {
-       String words[]=s.trim().split("\\s+");
-    //    StringBuilder reversed=new StringBuilder();
-    //    for(int i=words.length-1;i>=0;i--){
-    //     reversed.append(words[i]);
-    //     if(i!=0){
-    //         reversed.append(" ");
-    //     }
-    //    } 
-    //    return reversed.toString();
-    StringBuilder reversed=new StringBuilder();
-    for(int i=words.length-1;i>=0;i--){
-        reversed.append(words[i]);
-        if(i!=0){
-            reversed.append((" "));
+        // String []strs=s.trim().split("\\s+");
+        // StringBuilder res=new StringBuilder();
+        // for(int i=strs.length-1;i>=0;i--){
+        //     res.append(strs[i]);
+        //     if(i!=0){
+        //         res.append(" ");
+        //     }
+        // }
+        // return res.toString();
+
+        //using two pointer
+        //reverse whole string
+ s = s.trim().replaceAll("\\s+", " ");
+
+        // Step 2: reverse the entire string using StringBuilder
+        StringBuilder sb = new StringBuilder(s);
+        reverse(sb, 0, sb.length() - 1);
+
+        // Step 3: reverse each word
+        int n = sb.length();
+        int l = 0;
+        for (int r = 0; r <= n; r++) {
+            if (r == n || sb.charAt(r) == ' ') {
+                reverse(sb, l, r - 1);
+                l = r + 1;
+            }
         }
+
+        return sb.toString();
     }
-    return reversed.toString();
+     private void reverse(StringBuilder sb, int left, int right) {
+        while (left < right) {
+            char temp = sb.charAt(left);
+            sb.setCharAt(left, sb.charAt(right));
+            sb.setCharAt(right, temp);
+            left++;
+            right--;
+        }
     }
 }
