2f002d797d6015f3b0536d1b4c0995d09ecae9ec
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-09 10:13:58 +0530

    Time: 151 ms (5.06%), Space: 45.8 MB (10.04%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,48 +1,85 @@
-import java.util.HashSet;
-import java.util.Set;
+// import java.util.HashSet;
+// import java.util.Set;
 
-public class Solution {
-    public String reverseVowels(String s) {
-        // Set of vowels for quick lookup
-        Set<Character> vowels = new HashSet<>();
-        vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
-        vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
+// public class Solution {
+//     public String reverseVowels(String s) {
+//         // Set of vowels for quick lookup
+//         Set<Character> vowels = new HashSet<>();
+//         vowels.add('a'); vowels.add('e'); vowels.add('i'); vowels.add('o'); vowels.add('u');
+//         vowels.add('A'); vowels.add('E'); vowels.add('I'); vowels.add('O'); vowels.add('U');
         
-        // Convert the string to a character array for easier manipulation
-        char[] sArray = s.toCharArray();
-        int left = 0, right = sArray.length - 1;
+//         // Convert the string to a character array for easier manipulation
+//         char[] sArray = s.toCharArray();
+//         int left = 0, right = sArray.length - 1;
         
-        // Two-pointer approach
-        while (left < right) {
-            // Move the left pointer until we find a vowel
-            while (left < right && !vowels.contains(sArray[left])) {
-                left++;
-            }
-            // Move the right pointer until we find a vowel
-            while (left < right && !vowels.contains(sArray[right])) {
-                right--;
-            }
-            // Swap the vowels
-            char temp = sArray[left];
-            sArray[left] = sArray[right];
-            sArray[right] = temp;
+//         // Two-pointer approach
+//         while (left < right) {
+//             // Move the left pointer until we find a vowel
+//             while (left < right && !vowels.contains(sArray[left])) {
+//                 left++;
+//             }
+//             // Move the right pointer until we find a vowel
+//             while (left < right && !vowels.contains(sArray[right])) {
+//                 right--;
+//             }
+//             // Swap the vowels
+//             char temp = sArray[left];
+//             sArray[left] = sArray[right];
+//             sArray[right] = temp;
             
-            // Move both pointers inward
-            left++;
-            right--;
-        }
+//             // Move both pointers inward
+//             left++;
+//             right--;
+//         }
         
-        // Convert the character array back to a string and return
-        return new String(sArray);
+//         // Convert the character array back to a string and return
+//         return new String(sArray);
+//     }
+
+//     // public static void main(String[] args) {
+//     //     Solution sol = new Solution();
+        
+//     //     String result1 = sol.reverseVowels("hello");
+//     //     System.out.println(result1);  // Output: "holle"
+        
+//     //     String result2 = sol.reverseVowels("leetcode");
+//     //     System.out.println(result2);  // Output: "leotcede"
+//     // }
+// }
+
+
+//without charArray
+
+class Solution {
+    static boolean isVowel(char c){
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+                c=='A'||c=='E'||c=='I'||c=='O'||c=='U'){
+                    return true;
+                }
+                return false;
     }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        
-        String result1 = sol.reverseVowels("hello");
-        System.out.println(result1);  // Output: "holle"
-        
-        String result2 = sol.reverseVowels("leetcode");
-        System.out.println(result2);  // Output: "leotcede"
+    public String reverseVowels(String s) {
+        int n=s.length();
+        int i=0;
+        int j=n-1;
+        while(i<j){
+            if(!isVowel(s.charAt(i))){
+                i++;
+            }
+            else if(!isVowel(s.charAt(j))){
+                j--;
+            }
+            else{
+                StringBuilder sb = new StringBuilder(s);
+                char temp = sb.charAt(i);
+                sb.setCharAt(i, sb.charAt(j));
+                sb.setCharAt(j, temp);
+                i++;
+                j--;
+                s=sb.toString();
+            }
+        }
+        return s;
     }
 }
