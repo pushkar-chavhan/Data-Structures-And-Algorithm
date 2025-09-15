d5233f1cd784a8c625c7820cfc2f6114b0041379
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-15 10:08:14 +0530

    Time: 2 ms (88.19%), Space: 44.3 MB (80.72%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,26 +9,62 @@
  * }
  */
 class Solution {
+
+    ListNode mergeTwoSortedList(ListNode l1,ListNode l2){
+        if(l1==null){
+            return l2;
+        }
+        if(l2==null){
+            return l1;
+        }
+        if(l1.val<=l2.val){
+            l1.next=mergeTwoSortedList(l1.next,l2);
+            return l1;
+        }
+        else{
+            l2.next=mergeTwoSortedList(l1,l2.next);
+            return l2;
+        }
+    }
+    ListNode partitionAndMerge(int start,int end,ListNode[]list){
+        if(start==end){
+            return list[start];
+        }
+        if(start>end){
+            return null;
+        }
+        int mid= start+(end-start)/2;
+        ListNode l1=partitionAndMerge(start,mid,list);
+        ListNode l2=partitionAndMerge(mid+1,end,list);
+        return mergeTwoSortedList(l1,l2);
+    }
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null||lists.length==0) return null;
-        //sglyat pahle sglya list cha fist element queue mdhe insertkarach 
-        PriorityQueue<ListNode>heap=new PriorityQueue<>((a,b)->a.val-b.val);
-        for(ListNode node:lists){
-            if(node!=null){
-                heap.offer(node);
-            }
+        int n=lists.length;
+        if(n==0){
+            return null;
         }
-        //ek dummy list gheun tyat smaller element cha next elemet takayach heap mdhe
-        ListNode dummy=new ListNode(0);
-        ListNode current=dummy;
-        while(!heap.isEmpty()){
-            ListNode smallest=heap.poll();
-            current.next=smallest;
-            current=current.next;
-            if(smallest.next!=null){
-                heap.offer(smallest.next);
-            }
-        }
-        return dummy.next;
+        return partitionAndMerge(0,n-1,lists);
+
+        //using heap
+        // if(lists==null||lists.length==0) return null;
+        // //sglyat pahle sglya list cha fist element queue mdhe insertkarach 
+        // PriorityQueue<ListNode>heap=new PriorityQueue<>((a,b)->a.val-b.val);
+        // for(ListNode node:lists){
+        //     if(node!=null){
+        //         heap.offer(node);
+        //     }
+        // }
+        // //ek dummy list gheun tyat smaller element cha next elemet takayach heap mdhe
+        // ListNode dummy=new ListNode(0);
+        // ListNode current=dummy;
+        // while(!heap.isEmpty()){
+        //     ListNode smallest=heap.poll();
+        //     current.next=smallest;
+        //     current=current.next;
+        //     if(smallest.next!=null){
+        //         heap.offer(smallest.next);
+        //     }
+        // }
+        // return dummy.next;
     }
 }
