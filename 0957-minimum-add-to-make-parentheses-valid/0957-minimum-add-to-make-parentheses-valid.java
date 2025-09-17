1324d319c31d306762901b27e29818cb85ca5790
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-17 10:45:37 +0530

    Time: 0 ms (100%), Space: 41.7 MB (29.47%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.java
@@ -1,21 +1,42 @@
+// class Solution {
+//     public int minAddToMakeValid(String s) {
+//         // int openCount = 0;    // Counts unmatched '('
+//         // int closeNeeded = 0;  // Counts unmatched ')'
+
+//         // for (char c : s.toCharArray()) {
+//         //     if (c == '(') {
+//         //         openCount++;  // Increment unmatched '('
+//         //     } else if (c == ')') {
+//         //         if (openCount > 0) {  // Match ')' with '('
+//         //             openCount--;
+//         //         } else {
+//         //             closeNeeded++;  // No unmatched '(', so we need an extra ')'
+//         //         }
+//         //     }
+//         // }
+
+//         // // Total parentheses to add = unmatched '(' + unmatched ')'
+//         // return openCount + closeNeeded;
+
+//     }
+// }
 class Solution {
     public int minAddToMakeValid(String s) {
-        int openCount = 0;    // Counts unmatched '('
-        int closeNeeded = 0;  // Counts unmatched ')'
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                openCount++;  // Increment unmatched '('
-            } else if (c == ')') {
-                if (openCount > 0) {  // Match ')' with '('
-                    openCount--;
-                } else {
-                    closeNeeded++;  // No unmatched '(', so we need an extra ')'
+        int openCnt=0;
+        int closeNeed=0;
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                openCnt++;
+            }
+            else if(c==')'){
+                if(openCnt>0){
+                    openCnt--;
+                }
+                else{
+                    closeNeed++;
                 }
             }
         }
-
-        // Total parentheses to add = unmatched '(' + unmatched ')'
-        return openCount + closeNeeded;
+        return openCnt+closeNeed;
     }
 }
