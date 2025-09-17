b16a119a9e34c4228cfdf2c1e13793b80c31de4f
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-17 09:54:56 +0530

    Time: 2 ms (91.96%), Space: 41.6 MB (47.4%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,26 +1,46 @@
 public class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
+        // if (n == 1) return "1";
         
-        String prev = "1";
+        // String prev = "1";
         
-        for (int i = 2; i <= n; i++) {
-            StringBuilder nextSeq = new StringBuilder();
-            int count = 1;
+        // for (int i = 2; i <= n; i++) {
+        //     StringBuilder nextSeq = new StringBuilder();
+        //     int count = 1;
             
-            for (int j = 1; j < prev.length(); j++) {
-                if (prev.charAt(j) == prev.charAt(j - 1)) {
-                    count++;
-                } else {
-                    nextSeq.append(count).append(prev.charAt(j - 1));
-                    count = 1;
-                }
-            }
+        //     for (int j = 1; j < prev.length(); j++) {
+        //         if (prev.charAt(j) == prev.charAt(j - 1)) {
+        //             count++;
+        //         } else {
+        //             nextSeq.append(count).append(prev.charAt(j - 1));
+        //             count = 1;
+        //         }
+        //     }
             
-            nextSeq.append(count).append(prev.charAt(prev.length() - 1));
-            prev = nextSeq.toString();
+        //     nextSeq.append(count).append(prev.charAt(prev.length() - 1));
+        //     prev = nextSeq.toString();
+        // }
+        
+        // return prev;
+
+
+
+        //mik
+
+        if(n==1){
+            return "1";
         }
-        
-        return prev;
+        String say=countAndSay(n-1);
+        StringBuilder result=new StringBuilder();
+        for(int i=0;i<say.length();i++){
+            int count=1;
+            char ch=say.charAt(i);
+            while(i<say.length()-1&& say.charAt(i)==say.charAt(i+1)){
+                count++;
+                i++;
+            }
+            result.append(count).append(ch);
+        }
+        return result.toString();
     }
 }
