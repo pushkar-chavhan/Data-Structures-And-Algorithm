57b8253fee012c4fafb674690117ec297f339e41
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-20 17:40:31 +0530

    Time: 0 ms (100%), Space: 42.9 MB (30.49%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,40 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-       List<List<Integer>> ans = new ArrayList<>();
-
-        int n=nums.length;
-        int subset=1<<n;
-        for(int num=0;num<subset;num++){
-            List<Integer>li=new ArrayList<>();
-            for(int i=0;i<n;i++){
-                if((num & (1<<i))!=0){
-                    li.add(nums[i]);
-                }
-                
-            }
-            ans.add(li);
+    public void solve(int i,int[]nums,List<Integer>ds,List<List<Integer>>result){
+        if(i>=nums.length){
+            result.add(new ArrayList<Integer>(ds));
+            return;
         }
-        return ans;
+        ds.add(nums[i]);
+        solve(i+1,nums,ds,result);
+        ds.remove(ds.size()-1);
+        solve(i+1,nums,ds,result);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer>ds=new ArrayList<>();
+        solve(0,nums,ds,result);
+        return result;
+
+
+
+
+    //    List<List<Integer>> ans = new ArrayList<>();
+
+    //     int n=nums.length;
+    //     int subset=1<<n;
+    //     for(int num=0;num<subset;num++){
+    //         List<Integer>li=new ArrayList<>();
+    //         for(int i=0;i<n;i++){
+    //             if((num & (1<<i))!=0){
+    //                 li.add(nums[i]);
+    //             }
+                
+    //         }
+    //         ans.add(li);
+    //     }
+    //     return ans;
+
+
+
     }
 }
