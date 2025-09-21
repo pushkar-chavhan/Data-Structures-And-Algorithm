2df64460467ca61afb8e5d1b46df3d649585836f
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-22 00:14:18 +0530

    Time: 1 ms (61.11%), Space: 45.9 MB (93.7%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,43 +1,49 @@
+// class Solution {
+//     public int trap(int[] height) {
+//         int n=height.length;
+//         int mxl[]=new int[n];
+//         int mxr[]=new int[n];
+//         mxl[0]=height[0];
+//         for(int i=1;i<n;i++){
+//             mxl[i]=Math.max(mxl[i-1],height[i]);
+//         }
+//         mxr[n-1]=height[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             mxr[i]=Math.max(mxr[i+1],height[i]);
+//         }
+//         int water[]=new int[n];
+//         for(int i=0;i<n;i++){
+//             water[i]=Math.min(mxl[i],mxr[i])-height[i];
+//         }
+//         int sum=0;
+//         for(int i=0;i<n;i++){
+//             sum+=water[i];
+//         }
+//         return sum;
+//     }
+// }
+
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        // int[] left=new int [n];
-        // int [] right=new int [n];
-        // left[0]=height[0];
-        // for(int i=1;i<n;i++){//first travere from left and check left[i]>height[i]
-        //     left[i]=Math.max(left[i-1],height[i]);
-        // }
-        // right[n-1]=height[n-1];
-        // for(int i=n-2;i>=0;i--){//traversse from right
-        //     right[i]=Math.max(right[i+1],height[i]);
-        // }
-        // int ans=0;
-        // for(int i=0;i<n;i++){
-        //     ans+=Math.min(left[i],right[i])-height[i];
-        // }
-        // return ans;
-        int lmax = 0, rmax = 0, total = 0;
-        int l = 0, r = n - 1;
-        while (l < r) {
-            if (height[l] <= height[r]) {
-                if (lmax > height[l]) {
-                    total += lmax - height[l];
-                } else {
-                    lmax = height[l];
-
-                }
-                l += 1;
-            } else {
-                if (rmax > height[r]) {
-                    total += rmax - height[r];
-                } else {
-                    rmax = height[r];
-
-                }
-                r -= 1;
-            }
-
+        int n=height.length;
+        int mxl[]=new int[n];
+        int mxr[]=new int[n];
+        mxl[0]=height[0];
+        for(int i=1;i<n;i++){
+            mxl[i]=Math.max(mxl[i-1],height[i]);
         }
-        return total;
+        mxr[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            mxr[i]=Math.max(mxr[i+1],height[i]);
+        }
+        int water[]=new int[n];
+        for(int i=0;i<n;i++){
+            water[i]=Math.min(mxl[i],mxr[i])-height[i];
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=water[i];
+        }
+        return sum;
     }
 }
