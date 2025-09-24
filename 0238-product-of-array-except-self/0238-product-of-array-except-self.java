b5480ecd2257931e93686f1bc57f355c7e4c698c
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-24 17:15:25 +0530

    Time: 2 ms (87.16%), Space: 56.1 MB (26.02%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,21 +1,56 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int left[]=new int[nums.length];
-        int right[]=new int[nums.length];
-        int ans[]=new int[nums.length];
+        int n=nums.length;
+
+        //APP-1 =With division
+
+        // int productWithoutZero=1;
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==0){
+        //         count++;
+        //     }else{
+        //         productWithoutZero*=nums[i];
+        //     }
+        // }
+        // int result[]=new int[nums.length];
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         if(count>0){
+        //             result[i]=0;
+        //         }
+        //         else{
+        //             result[i]=productWithoutZero/nums[i];
+        //         }
+        //     }
+        //     else{
+        //         if(count>1){
+        //             result[i]=0;
+        //         }
+        //         else{
+        //             result[i]=productWithoutZero;
+        //         }
+        //     }
+        // }
+        // return result;
+
+
+        //Approach 2:without division but use extra spaces
+        
+        int left[]=new int[n];
         left[0]=1;
-        for(int i=1;i<nums.length;i++){
+        int right[]=new int[n];
+        right[n-1]=1;
+        for(int i=1;i<n;i++){
             left[i]=left[i-1]*nums[i-1];
         }
-        right[nums.length-1]=1;
-        for(int i=nums.length-2;i>-1;i--){
+        for(int i=n-2;i>=0;i--){
             right[i]=right[i+1]*nums[i+1];
         }
-        for(int i=0;i<nums.length;i++){
-            ans[i]=left[i]*right[i];
+        int result[]=new int[n];
+        for(int i=0;i<n;i++){
+            result[i]=left[i]*right[i];
         }
-        return ans;
+        return result;
     }
-
-
 }
