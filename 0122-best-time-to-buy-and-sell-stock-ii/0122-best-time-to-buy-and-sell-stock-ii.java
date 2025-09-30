61327b196c5175052f2b46f45eb00a03fb19e1de
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-09-30 22:12:38 +0530

    Time: 1 ms (77.89%), Space: 47.1 MB (7.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,13 +1,31 @@
 import java.util.*;
 //very easy
 class Solution {
-    public int maxProfit(int[] nums) {
-        int maxProfit=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
-                maxProfit+=nums[i]-nums[i-1];
-            }
+    int t[];
+    int solve(int nums[],int i){
+        if(i<=0){
+            return 0;
         }
-        return maxProfit;
+        if(t[i]!=-1){
+            return t[i];
+        }
+        int profit=solve(nums,i-1);
+        if(nums[i]>nums[i-1]){
+            profit+=nums[i]-nums[i-1];
+        }
+        return t[i]= profit;
+    }
+    public int maxProfit(int[] nums) {
+        int n=nums.length;
+        t=new int[n+1];
+        Arrays.fill(t,-1);
+        return solve(nums,n-1);
+        // int maxProfit=0;
+        // for(int i=1;i<nums.length;i++){
+        //     if(nums[i]>nums[i-1]){
+        //         maxProfit+=nums[i]-nums[i-1];
+        //     }
+        // }
+        // return maxProfit;
     }
 }
