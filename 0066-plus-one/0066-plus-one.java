e925cd60c50a6a36035f0edc85c7bcc3858694d2
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-10-02 21:18:38 +0530

    Time: 0 ms (100%), Space: 41.9 MB (68.97%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,18 +1,32 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n=digits.length;
-        for(int i=n-1;i>=0;i--){
-            //if the current digit is less than just increment and return it
+        // List<Integer> res = new ArrayList<>();
+        // String dumm = "";
+        // for (int num : digits) {
+        //     dumm += num;
+        // }
+        // long plus = Long.parseLong(dumm) + 1;
+        // String ans = String.valueOf(plus);
+        // for (char c : ans.toCharArray()) {
+        //     res.add(c - '0');
+        // }
+        // int[] result = new int[res.size()];
+        // for (int i = 0; i < res.size(); i++) {
+        //     result[i] = res.get(i);
+        // }
+        // return result;
+
+        //best approach:
+        for(int i=digits.length-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
                 return digits;
             }
-            //if digit is 9 set it to 0
             digits[i]=0;
         }
-        //If all the digits were 9, we need a new array with an extra digit
-        int[] newdigits=new int[n+1];
-        newdigits[0]=1;//set first digit as 1 other will remain 0 by default
-        return newdigits;
+        // If all digits were 9, create new array [1,0,0,...,0]
+        int res[]=new int[digits.length+1];
+        res[0]=1;
+        return res;
     }
 }
