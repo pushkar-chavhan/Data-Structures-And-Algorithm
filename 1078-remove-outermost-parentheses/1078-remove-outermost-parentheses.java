04982d97c68cab3650b88361a467b976d4b70b95
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-10-07 23:33:12 +0530

    Time: 2 ms (99.03%), Space: 42.1 MB (89.74%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.java
@@ -17,41 +17,58 @@ class Solution {
         //     }
         // }
         // return ans.toString();
-        
+
         //second approach
-    StringBuilder ans=new StringBuilder();
-    int count=0;
-    for(char ch:s.toCharArray()){
-        if(ch==')'){
-            count--;
-        }
-        if(count!=0){
-            ans.append(ch);
-        }
-        if(ch=='('){
-            count++;
-        }
-    }
-    return ans.toString();
+        // StringBuilder ans=new StringBuilder();
+        // int count=0;
+        // for(char ch:s.toCharArray()){
+        //     if(ch==')'){
+        //         count--;
+        //     }
+        //     if(count!=0){
+        //         ans.append(ch);
+        //     }
+        //     if(ch=='('){
+        //         count++;
+        //     }
+        // }
+        // return ans.toString();
 
         //using stack
-    //     Stack<Character>st=new Stack<>();
-    //     StringBuilder ans=new StringBuilder();
-    //     for(char ch:s.toCharArray()){
-    //         if(ch=='('){
-    //             if(!st.isEmpty()){
-    //                 ans.append(ch);
-    //             }
-    //             st.push(ch);
-    //         }
-    //         else if(ch==')'){
-    //             st.pop();
-    //             if(!st.isEmpty()){
-    //                 ans.append(ch);
-    //             }
-    //         }   
-    //     }
-    //     return ans.toString();
-    
-     }
+        //     Stack<Character>st=new Stack<>();
+        //     StringBuilder ans=new StringBuilder();
+        //     for(char ch:s.toCharArray()){
+        //         if(ch=='('){
+        //             if(!st.isEmpty()){
+        //                 ans.append(ch);
+        //             }
+        //             st.push(ch);
+        //         }
+        //         else if(ch==')'){
+        //             st.pop();
+        //             if(!st.isEmpty()){
+        //                 ans.append(ch);
+        //             }
+        //         }   
+        //     }
+        //     return ans.toString();
+
+        //Striver method
+
+        StringBuilder ans = new StringBuilder();
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+
+                if (count > 0)
+                    ans.append('(');
+                count++;
+            } else if (c == ')') {
+                count--;
+                if (count > 0)
+                    ans.append(')');
+            }
+        }
+        return ans.toString();
+    }
 }
