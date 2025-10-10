0fed6b3e7c721ec6871a9f7bbe8d199797356fc2
Pushkar Chavhan <155805433+pushkar-chavhan@users.noreply.github.com>
2025-10-11 00:59:35 +0530

    Time: 2 ms (97.93%), Space: 60.1 MB (73.79%) - LeetHub

--- 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
+++ 3383-taking-maximum-energy-from-the-mystic-dungeon/3383-taking-maximum-energy-from-the-mystic-dungeon.java
@@ -1,25 +1,40 @@
+// class Solution {
+//     int n;
+//     int K;
+//     private int solve(int i,int[]energy,int []t){
+//         if(i>=n){
+//             return 0;
+//         }
+//         if(t[i]!=Integer.MIN_VALUE){
+//             return t[i];
+//         }
+//         t[i]=energy[i]+solve(i+K,energy,t);
+//         return t[i];
+//     }
+//     public int maximumEnergy(int[] energy, int k) {
+//         n=energy.length;
+//         K=k;
+//         int[]t=new int[n];
+//         Arrays.fill(t,Integer.MIN_VALUE);
+//         int maxEnergy=Integer.MIN_VALUE;
+//         for(int i=0;i<n;i++){
+//             maxEnergy=Math.max(maxEnergy,solve(i,energy,t));
+//         }
+//         return maxEnergy;
+//     }
+// }
 class Solution {
-    int n;
-    int K;
-    private int solve(int i,int[]energy,int []t){
-        if(i>=n){
-            return 0;
-        }
-        if(t[i]!=Integer.MIN_VALUE){
-            return t[i];
-        }
-        t[i]=energy[i]+solve(i+K,energy,t);
-        return t[i];
-    }
     public int maximumEnergy(int[] energy, int k) {
-        n=energy.length;
-        K=k;
-        int[]t=new int[n];
-        Arrays.fill(t,Integer.MIN_VALUE);
-        int maxEnergy=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            maxEnergy=Math.max(maxEnergy,solve(i,energy,t));
+        int max = Integer.MIN_VALUE;
+        for (int i=energy.length-k;i<energy.length;i++) {
+            max = Math.max(max, energy[i]);
         }
-        return maxEnergy;
+        
+        for (int i=energy.length-k-1;i>=0;i--) {
+            energy[i] += energy[i+k];
+            max = Math.max(max, energy[i]);
+        }
+        return max;
+        
     }
 }
