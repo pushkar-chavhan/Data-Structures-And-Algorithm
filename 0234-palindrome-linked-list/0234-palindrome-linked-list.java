0f23dadaf5a14ecd44ecdb7768fc92d6261144a9
Chavhan Pushkar Anil <155805433+pushkar-chavhan@users.noreply.github.com>
2025-12-09 16:26:15 +0530

    Time: 3 ms (99.72%), Space: 94.4 MB (34.02%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,37 +1,34 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find middle using slow & fast pointer
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        ListNode prev=null;
-        while(slow!=null){
-            ListNode temp=slow.next;
-            slow.next=prev;
-            prev=slow;
-            slow=temp;
+
+        // Step 2: Reverse second half
+        ListNode prev = null;
+        while (slow != null) {
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
         }
-        ListNode first=head;
-        ListNode second=prev;
-        while(second!=null){
-            if(first.val!=second.val){
+
+        // Step 3: Compare first and second half
+        ListNode first = head;
+        ListNode second = prev;
+        while (second != null) {        // check all nodes from second half
+            if (first.val != second.val)
                 return false;
-            }
-            first=first.next;
-            second=second.next;
+            first = first.next;
+            second = second.next;
         }
+
         return true;
     }
 }
